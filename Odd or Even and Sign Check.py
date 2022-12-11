9c404b939cbeb69d8c94c8067c51fd8050ec42fb
Sarvan-Chowdary <118923259+Sarvan-Chowdary@users.noreply.github.com>
2022-12-11 12:32:26 +0530

    Add files via upload

---
+++ Odd or Even and Sign Check.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a Number: "))
+if a<0:
+    print(a,"is a negative number")
+elif a==0:
+    print(a,"is Zero")
+else:
+    print(a,"is positive")
+if a%2==0:
+    print(a,"is even")
+else:
+    print(a,"is odd")
